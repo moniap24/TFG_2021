7da939eb4d6b8ff32f09cfdb13a343b25c529bec
moniap24 <74410078+moniap24@users.noreply.github.com>
2021-09-06 01:50:48 +0200

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,12 @@
+% DEMO
+
+img1 = imread('/Users/monica/Documents/TFG/IMAGENES/PNOAMA/LR/256x256/PNOA_MA_Molino_256x256.png');
+img2 = imread('/Users/monica/Documents/TFG/IMAGENES/PNOAMA/MEJORES RESULTADOS/SRFBN/1_SRFBN-S_x4_BI/PNOA_MA_Molino_SR.png');
+
+fig = figure(); 
+ax(1) = axes('Units','normalized','Position', [ .1 .1 .4 .8]);
+ax(2) = axes('Units','normalized','Position', [ .525 .1 .4 .8]);
+
+imshow(img1, 'Parent', ax(1))
+imshow(img2, 'Parent', ax(2))
+linkaxes(ax)
